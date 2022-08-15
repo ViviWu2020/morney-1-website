fac888bc6c40aeee3a3fe499995de8eb316303bc
Vivi <jiahui1001@yeah.net>
2022-08-15 21:24:29 +0800

    deploy

---
+++ precache-manifest.7d5be2f77eb7e49b54fab3f3f18ff2bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc18d8d4b8cc4798b41",
+    "url": "/morney-1-website/css/app.2c088467.css"
+  },
+  {
+    "revision": "1ae1e6f13947fe8d456eb540c996e753",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "3fc18d8d4b8cc4798b41",
+    "url": "/morney-1-website/js/app.89dfd5a5.js"
+  },
+  {
+    "revision": "4ebd838a0652782080e8",
+    "url": "/morney-1-website/js/chunk-vendors.3313d488.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "9a67c30608d05f77de1e21f12eba5b93",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
